8fbf9590d3f65fbc4b49aa1ad2819f9bc7e72992
Adhityaa C <c.adhityaa@gmail.com>
2016-06-13 20:13:52 +0530

    Add language based category generation

---
+++ language_generate.py
@@ -0,0 +1,44 @@
+import os
+import inspect
+
+from coalib.settings.ConfigurationGathering import load_configuration
+from coalib.misc.DictUtilities import inverse_dicts
+from coalib.collecting.Collectors import (
+    collect_all_bears_from_sections, filter_section_bears_by_languages)
+from pyprint.NullPrinter import NullPrinter
+from coalib.output.printers.LogPrinter import LogPrinter
+
+def get_bears():
+    """
+    Get a dict of bears with the bear class as key.
+
+    :return:
+        A dict with bear classes as key and the list of sections
+        as value.
+    """
+    log_printer = LogPrinter(NullPrinter())
+    sections, _ = load_configuration(None, log_printer)
+    local_bears, global_bears = collect_all_bears_from_sections(
+        sections, log_printer)
+    return inverse_dicts(local_bears, global_bears)
+
+if __name__ == "__main__":
+    lang_dict = {}
+    for bear in get_bears():
+        for language in bear.supported_languages:
+            if language in lang_dict:
+                lang_dict[language].append(bear.name)
+            else:
+                lang_dict[language] = [bear.name]
+
+    with open("languages.rst", "w") as lang_file:
+        lang_file.write("**Supported Languages**\n-----------------------\n\n"
+                        ".. contents::\n"
+                        "    :local:\n"
+                        "    :depth: 1\n"
+                        "    :backlinks: none\n\n")
+        for language in lang_dict:
+            lang_file.write(language + "\n" + "=" * len(language) + "\n")
+            for bear in lang_dict[language]:
+                lang_file.write("* `" + bear + " <docs/" + bear + ".rst>`_\n")
+            lang_file.write("\n")
