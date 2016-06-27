16cb6645c1d330b9641c824baf379f6ede9dc05a
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2016-06-27 20:56:18 +0200

    generate_bear_csv: Make it better

--- generate_bear_csv.py
+++ generate_bear_csv.py
@@ -1,15 +1,11 @@
-from itertools import chain
-
 from bear_docs_utils import get_bears
 
 if __name__ == '__main__':
     language_can_detect = {}
     language_can_fix = {}
     can_detects = set()
-    can_fixes = set()
     for bear in get_bears():
         can_detects |= bear.can_detect
-        can_fixes |= bear.CAN_FIX
 
         for language in sorted(bear.LANGUAGES):
             if language in language_can_detect:
@@ -19,15 +15,20 @@ if __name__ == '__main__':
                 language_can_detect[language] = set(bear.can_detect)
                 language_can_fix[language] = set(bear.CAN_FIX)
 
-    columns = [val for val in chain(sorted(can_detects),
-                                    sorted("FIX "+fix for fix in can_fixes))]
+    columns = [val for val in sorted(can_detects)]
     file = ["Languages," + ','.join(columns) + '\n']
     for language in sorted(language_can_detect):
-        detects = ','.join('1' if val in language_can_detect[language] else '0'
+        def get_val(val):
+            if val in language_can_fix[language]:
+                return 'Can fix'
+
+            if val in language_can_detect[language]:
+                return 'Can detect'
+
+            return "Unsupported"
+        detects = ','.join(get_val(val)
                            for val in sorted(can_detects))
-        fixes = ','.join('1' if val in language_can_fix[language] else '0'
-                         for val in sorted(can_fixes))
-        file.append(language + ',' + detects + ',' + fixes + '\n')
+        file.append(language + ',' + detects + '\n')
     with open("language_support.csv", "w") as filehandle:
         filehandle.writelines(file)
 
@@ -57,11 +58,11 @@ if __name__ == '__main__':
                    for original_cat in language_can_detect[language]):
                 if any(cat == mapping[original_cat]
                        for original_cat in language_can_fix[language]):
-                    return "2"
+                    return "Can fix"
 
-                return "1"
+                return "Can detect"
 
-            return "0"
+            return "Unsupported"
 
         detects = ','.join(ret_number(cat) for cat in categories)
         shortfile.append(language + ',' + detects + '\n')
